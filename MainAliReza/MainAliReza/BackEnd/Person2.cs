f881e2aca364d05d10c5cae0dffe6763feb19f32
Ali <1a9n9f9@gmail.com>
2019-06-11 16:21:46 +0430

    p1

--- MainAliReza/MainAliReza/BackEnd/Person2.cs
+++ MainAliReza/MainAliReza/BackEnd/Person2.cs
@@ -16,7 +16,7 @@ namespace MainAliReza.BackEnd
             using (var db01 = new ConnectingDB())
             {
                 Person per = new Person
-                { FirstName = pp[0], LastName = pp[1], Birthday_Day = pp[2], Birthday_Month = pp[3], Birthday_Year = pp[4], E_Mail = pp[5], Gender = pp[9], Mobil = pp[6], PassWord = pp[11], UserName = pp[10], Picture = pp[12], Telephon = pp[7], Sath_E_Dastresy = "0" };
+                { FirstName = pp[0], LastName = pp[1], Birthday_Day = pp[2], Birthday_Month = pp[3], Birthday_Year = pp[4], E_Mail = pp[5], Gender = pp[9], Mobil = pp[6], PassWord = pp[11], UserName = pp[10], Picture = pp[12], Telephon = pp[7], Sath_E_Dastresy = pp[8] };
 
                 db01.person1.Add(per);
                 db01.SaveChanges();
